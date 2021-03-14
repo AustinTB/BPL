45bdf2bb138d88d2f5acdfc9c82a39c660e9ee79
austin <BaneyAT@gmail.com>
2021-03-14 16:52:59 -0400

    dynamic header title

--- homepage.php
+++ homepage.php
@@ -4,7 +4,7 @@
   <meta charset="utf-8">   
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">  
-  <title>BPL</title>
+  <title>BPL - Home</title>
  
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
   <style>
@@ -20,7 +20,9 @@
     footer { position: absolute; bottom: 0; width: 100%; height: 50px; color: WhiteSmoke; padding: 10px; }
    </style>   
 </head>
+
 <body>
   <?php include('header.html') ?>
 </body>
+
 </html>
