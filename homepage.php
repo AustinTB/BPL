cb95e9e05613cc212176a9975ce4d0d77cba6a00
austin <BaneyAT@gmail.com>
2021-03-14 21:02:12 -0400

    homepage

--- homepage.php
+++ homepage.php
@@ -1,14 +1,76 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-  <meta charset="utf-8">   
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1">  
+<head> 
   <title>BPL - Home</title>
+  <style>
+    .grid-container {
+      display: grid;
+      padding-bottom: 40px;
+      width: 100%;
+    }
+    .grid-header {
+      background-color: goldenrod;
+      width: 100%;
+    }
+    .grid-row {
+      position: relative;
+      top: 25px;
+      border-radius: 25px;
+      border-style: solid;
+      background-color: #ba8dba;
+      text-align: center;
+      padding-bottom: 20px;
+      width: 75%;
+    }
+    .btn-grid {
+      height: 35%;
+      min-width: 20%;
+      color: white;
+      background-color: purple;
+      border-radius: 25px;
+    }
+  </style>
 </head>
 
 <body>
   <?php include('header.html') ?>
+
+  <div class="grid-container">
+    <div class="grid-header">
+      <h1>Upcoming Matches</h1>
+    </div>
+    <div class="grid-row">
+      <h2>BPL 2021 - 4/17/21</h2>
+      <h3>VS. The Blue Mountains</h3>
+      <button class="btn-grid">
+        <a href="#"></a>
+        View Match
+      </button>
+    </div>
+    <div class="grid-row">
+      <h2>Exhibition - 4/19/21</h2>
+      <h3>VS. The Dudes</h3>
+      <button class="btn-grid">
+        <a href="#"></a>
+        View Match
+      </button>
+    </div>
+  </div>
+
+  <div class="grid-container">
+    <div class="grid-header">
+      <h1>My Teams</h1>
+    </div>
+    <div class="grid-row">
+      <h2>The Three Bucketeers</h2>
+      <h3>League: BPL</h3>
+      <button class="btn-grid">
+        <a href="#"></a>
+        View Team
+      </button>
+    </div>
+  </div>
+
 </body>
 
 </html>
