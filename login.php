8cba856ed5f26eb91164fd0e566decae8510fd15
austin <BaneyAT@gmail.com>
2021-04-12 21:28:49 -0400

    Login & signup WIP

--- login.php
+++ login.php
@@ -7,51 +7,48 @@
 
 <body>
     <?php include('header.html') ?>
+    <?php session_start(); ?>
 
     <div class="grid-container">
         <div class="grid-header">
-            <h1>Log In</h1>
+            <h1>Log in</h1>
         </div>
         <div class="grid-row">
-            <form action="success.php" onsubmit="return (validateInfo())">
+            <form action="<?php $_SERVER['PHP_SELF'] ?>" method="post" onsubmit="return (validateInfo())">
                 <h3>Username: </h3> 
-                <input type="text" id="username" class="grid-input" autofocus required />
+                <input type="text" name="username" class="grid-input" autofocus required />
                 <div id="user-msg" class="feedback"></div> 
                 <br/>
                 <h3>Password: </h3> 
-                <input type="password" id="pwd" class="grid-input" required />
+                <input type="password" name="pwd" class="grid-input" required />
                 <div id="pwd-msg" class="feedback"></div> 
                 <br/>
                 <input type="submit" class="btn-grid" value="Log in" />   <!-- use input type="submit" with the required attribute -->
             </form>
+            </br>
+            Don't have an account yet? <a href="signup.php">Sign up</a>
+            </br>
         </div>
     </div>
 
-    <script>
-        var user = document.getElementById("username");
+    <?php
+    // Verify the given credentials, and return true if they match an existing user.
+    function verify_credentials($user, $pwd) {
+        return true;
+    }
 
-        user.addEventListener('blur', function() {
-            checkUsername(8);
-        }, false);
+    if ($_SERVER['REQUEST_METHOD'] == 'POST' && strlen($_POST['username']) > 0) {
+        $pwd = htmlspecialchars($_POST['pwd']);
+        $user = htmlspecialchars($_POST['username']);
 
-        function checkUsername(minlength) {
-            var msg = document.getElementById("user-msg");
-            if (user.value.length < minlength && user.value.length > 0)
-                msg.textContent = "Username is too short";
-            else
-                msg.textContent = "";
-            }
-
-        function validateInfo() {
-            var pwd = document.getElementById("pwd").value;       
-            if (user.value.length < 8)
-            {    	  
-                document.getElementById("user-msg").innerHTML = "Username is too short";
-                document.getElementById("username").focus();
-                return false;
-            }
-            return true;
-        } 
-    </script>
+        if (verify_credentials($user, $pwd)) {
+            $_SESSION['user'] = $pwd;
+            $_SESSION['pwd'] = $user;
+            header('Location: success.php');
+        } else {
+            // error msg
+        }
+    } 
+    ?>
 </body>
 </html>
